72058e0de680764a6b59b2c7aa9657958f142ccc
anmol950915 <anmol950915@gmail.com>
2022-09-14 15:12:36 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-tydps6-evqcig
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-tydps6-evqcig)
---
+++ src/app/product-list/product-list.component.ts
@@ -0,0 +1,26 @@
+import { Component, Input } from '@angular/core';
+
+import { products } from '../products';
+
+@Component({
+  selector: 'app-product-list',
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css'],
+})
+export class ProductListComponent {
+  products = products;
+
+  buy() {
+    window.alert('The product has been bought!');
+  }
+
+  onNotify() {
+    window.alert('You will be notified once sale is live');
+  }
+}
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://angular.io/license
+*/
