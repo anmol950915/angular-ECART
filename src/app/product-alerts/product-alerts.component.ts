2590a504f8fc4f655bd65346cf3af942f8addee8
anmol950915 <anmol950915@gmail.com>
2022-09-14 15:58:40 +0530

    2 files modified

--- src/app/product-alerts/product-alerts.component.ts
+++ src/app/product-alerts/product-alerts.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { Product } from '../products';
+import { products } from '../products';
 
 @Component({
   selector: 'app-product-alerts',
@@ -9,5 +10,20 @@ import { Product } from '../products';
 export class ProductAlertsComponent {
   @Input() product!: Product;
   @Output() notify = new EventEmitter();
-  btnmsg = 'Notify Me';
+  constructor() {
+    this.changebuttonmsg();
+  }
+  btnmsg = 'abc';
+
+  changebuttonmsg() {
+    for (let product of products) {
+      if (product.onsale && !product.instock) {
+        this.btnmsg = 'Advance book';
+        alert('Hi');
+      } else {
+        this.btnmsg = 'Notify Me';
+        alert('Hi there');
+      }
+    }
+  }
 }
